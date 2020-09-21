1141733fc25de3cee1823aefccde845e0fb49add
ShlomoLibo <49692869+ShlomoLibo@users.noreply.github.com>
2020-09-21 23:15:41 +0300

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -7,7 +7,7 @@ DE_FIELD = data.Field(init_token="<SOS>", eos_token="<EOS>", pad_token="<PAD>", 
 
 
 def get_data(batch_size) -> Tuple[Iterator, Iterator, Iterator]:
-    train_data, validation_data, test_data = datasets.WMT14.splits(exts=(".de", ".en"), fields=(DE_FIELD, EN_FIELD))
+    train_data, validation_data, test_data = datasets.Multi30k.splits(exts=(".de", ".en"), fields=(DE_FIELD, EN_FIELD))
     EN_FIELD.build_vocab(train_data, min_freq=2)
     DE_FIELD.build_vocab(train_data, min_freq=2)
     return data.BucketIterator.splits((train_data, validation_data, test_data), batch_size=batch_size)
